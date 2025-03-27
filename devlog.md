83e0f6b61e7eaa04f14d27436412891dd5774a56
Ben Stidham <ben.stidham1@gmail.com>
2025-03-27 12:11:09 -0500

    got input working

---
+++ devlog.md
@@ -0,0 +1,5 @@
+# Dev Log
+
+3/27/25:11:30-12:00
+Got line input working. Was a bit messy (Had an unnecessary parentheses around some elements that was being evil)
+using the prompt? function provided, implemented it
--- prefix.rkt
+++ prefix.rkt
@@ -1,4 +1,6 @@
 #lang racket
+
+; Created by prof, provided for assignment as best I can tell
 (define prompt?
    (let [(args (current-command-line-arguments))]
      (cond
@@ -6,3 +8,15 @@
        [(string=? (vector-ref args 0) "-b") #f]
        [(string=? (vector-ref args 0) "--batch") #f]
        [else #t])))
+
+
+(define (line_input line_lst)
+  (when prompt?
+    (display "Prefix Operation: "))
+  (define input_line (read-line (current-input-port) 'any))
+  (if (not (equal? input_line "quit"))
+      (line_input (append line_lst (list input_line)))
+      line_lst
+  ))
+
+(line_input '())

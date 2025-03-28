1a37472e26701214e6dcc1ef8f4cd6f03b0dedd3
Ben Stidham <ben.stidham1@gmail.com>
2025-03-28 18:36:33 -0500

    just saving progress

--- prefix.rkt
+++ prefix.rkt
@@ -20,7 +20,7 @@
 (define (string-char str)
   (if (equal? (string-length str) 1)
     (string-ref str 0)
-    (error "Not a char")))
+    (error "Error: Not a valid input")))
 
 ; Checks if char is one of our operators (+, -, *, /)
 (define (operator? ch) (if (equal? (string-length ch) 1)
@@ -40,13 +40,16 @@
     #f)
   #f))
 
+; Check if is a valid element
 (define (valid_element? str)
   (if (prev_ref? str) #t
     (if (is-number? str) #t
       (if (operator? str) #t #f))))
 
+; Validate the whole line
 (define (validate_line_elements line_lst) (andmap valid_element? line_lst))
 
+; Split the line properly
 (define (split_line line)
   (filter non-empty-string?
     (string-split 
@@ -61,6 +64,32 @@
           "/" " / ")
           "$" " $") " ")))
 
+; get the reference val
+(define (get_ref ref line_lst)
+  (if (< (string->number (substring ref 1 (string-length ref)) (length line_lst)))
+    (string->number (substring ref 1 (string-length ref)))
+    (error "Error: Invalid reference to previous result")))
+
+; Replace the $num instances with values
+(define (replace_refs current_line line_lst)
+  (if (prev_ref? (car current_line))
+    (append (get_ref (car current_line) (replace_refs (cdr current_line))))
+    (append (car current_line) (replace_refs (cdr current_line)))))
+
+; Evaluate recursively
+(define (eval_recurs current_line numbers)
+  (if (empty? current_line))
+    (if (equal? (length numbers) 1)
+      (car numbers)
+      (error "Error: More numbers than operators")))
+
+; Evaluate the current line
+(define (eval_line current_line line_lst)
+  (define operators '())
+  (define numbers '())
+  )
+
+; Get user input
 (define (line_input line_lst)
   (when prompt?
     (display "Prefix Operation: "))

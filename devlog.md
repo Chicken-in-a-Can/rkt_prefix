1afe461e4dd4a2f18cb35e510dcde1d3e8df3aa2
Ben Stidham <ben.stidham1@gmail.com>
2025-03-28 14:22:16 -0500

    added checking line characters

--- devlog.md
+++ devlog.md
@@ -1,8 +1,11 @@
 # Dev Log
 
-3/27/25:11:30-12:00
-Got line input working. Was a bit messy (Had an unnecessary parentheses around some elements that was being evil)
-using the `prompt?` function provided, implemented it
+### 3/27/25:11:30-12:00  
+Got line input working. Was a bit messy (Had an unnecessary parentheses around some elements that was being evil)  
+using the `prompt?` function provided, implemented it  
 
-3/27/25:12:00-12:30
-Now splits the line input into sub arrays properly. Whitespace also works well with this
+### 3/27/25:12:00-12:30  
+Now splits the line input into sub arrays properly. Whitespace also works well with this  
+
+### 3/28/25:12:00-12:30  
+Now properly checks if the line has correct characters  
--- prefix.rkt
+++ prefix.rkt
@@ -9,6 +9,44 @@
        [(string=? (vector-ref args 0) "--batch") #f]
        [else #t])))
 
+; Checks if given character is any in list
+(define (is-any ch lst)
+  (cond
+    [(null? lst) #f]
+    [(char=? ch (car lst)) #t]
+    [else (is-any ch (cdr lst))]))
+
+; Converts char to string, gives error if not
+(define (string-char str)
+  (if (equal? (string-length str) 1)
+    (string-ref str 0)
+    (error "Not a char")))
+
+; Checks if char is one of our operators (+, -, *, /)
+(define (operator? ch) (if (equal? (string-length ch) 1)
+  (is-any (string-char ch) '(#\+ #\- #\* #\/))
+  #f))
+; Checks if element is a number
+(define (is-number? str)
+  (if (char-numeric? (string-ref str 0))
+    (if (> (string-length str) 1)
+      (is-number? (substring str 1 (string-length str)))
+      #t)
+    #f))
+; Checks if element is  reference to prior output
+(define (prev_ref? str) (if (equal? (string-ref str 0) #\$)
+  (if (is-number? (substring str 1 (string-length str)))
+    #t
+    #f)
+  #f))
+
+(define (valid_element? str)
+  (if (prev_ref? str) #t
+    (if (is-number? str) #t
+      (if (operator? str) #t #f))))
+
+(define (validate_line_elements line_lst) (andmap valid_element? line_lst))
+
 (define (split_line line)
   (filter non-empty-string?
     (string-split 
@@ -17,18 +55,18 @@
         string-replace (
         string-replace (
         string-replace line
-        "+" " + ")
-        "-" " - ")
-        "*" " * ")
-        "/" " / ")
-        "$" " $") " ")))
+          "+" " + ")
+          "-" " - ")
+          "*" " * ")
+          "/" " / ")
+          "$" " $") " ")))
 
 (define (line_input line_lst)
   (when prompt?
     (display "Prefix Operation: "))
   (define input_line (read-line (current-input-port) 'any))
   (if (not (equal? input_line "quit"))
-      (line_input (append line_lst (list (split_line input_line))))
+      (line_input (append line_lst (list (validate_line_elements (split_line input_line)))))
       line_lst
   ))
 
